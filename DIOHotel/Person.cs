84763898c818dd04437a493e7c47b4bd7c6b2bba
gabriel <gabriel.gregorio1987@gmail.com>
2023-04-25 20:55:44 -0300

    Creating a Person class

---
+++ DIOHotel/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIOHotel
+{
+    internal class Person
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+
+        public Person() {}
+
+        public Person(string? name, string? lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+    }
+}
