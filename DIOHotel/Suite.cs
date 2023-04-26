cf3d67715b23580935cbb5e5c62c8cc3f10476c2
gabriel <gabriel.gregorio1987@gmail.com>
2023-04-25 21:09:56 -0300

    Creating a Suite class

---
+++ DIOHotel/Suite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIOHotel
+{
+    internal class Suite
+    {
+        public string? SuiteName { get; set; } 
+        public string? Capacity { get; set; }
+        public decimal DailyValue { get; set; }
+
+        public Suite() {}
+
+        public Suite(string? suiteName, string? capacity, decimal dailyValue)
+        {
+            SuiteName = suiteName;
+            Capacity = capacity;
+            DailyValue = dailyValue;
+        }
+    }
+}
