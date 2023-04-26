5b75718d7ea38c84061c536146eecfa51e025149
gabriel <gabriel.gregorio1987@gmail.com>
2023-04-25 21:14:51 -0300

    Crating Reserve class

---
+++ DIOHotel/Reserve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIOHotel
+{
+    internal class Reserve
+    {
+        public List<Client>? Clients;
+
+        public Suite? Suite;
+        public int ReservedDays { get; set; }
+
+        public Reserve() {}
+
+        public Reserve(List<Client>? clients, Suite? suite, int reservedDays)
+        {
+            Clients = clients;
+            Suite = suite;
+            ReservedDays = reservedDays;
+        }
+    }
+}
