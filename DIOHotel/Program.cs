90fa652abfdfa0091c78b5e747760f0356cf2e0b
gabriel <gabriel.gregorio1987@gmail.com>
2023-04-27 18:02:30 -0300

    Refactoring the Program.cs and fixing the for

--- DIOHotel/Program.cs
+++ DIOHotel/Program.cs
@@ -11,90 +11,85 @@ Console.WriteLine("2 - Suite \n");
 
 int add = int.Parse(Console.ReadLine());
 
-while(add != 1 && add != 2)
+while (add != 1 && add != 2)
 {
     Console.WriteLine("Press 1 or 2");
     add = int.Parse(Console.ReadLine());
 }
 
 
-add = 1;
-if (add == 1)
+Client client = new Client();
+Reserve reserve = new Reserve();
+int allClients = 0;
+
+while (add == 1)
 {
-    Client client = new Client();
-    Reserve reserve = new Reserve();
+    Console.WriteLine("Name");
+    client.Name = Console.ReadLine();
+    Console.WriteLine("\n");
 
-    while (add == 1)
-    {
-        Console.WriteLine("Name");
-        client.Name = Console.ReadLine();
-        Console.WriteLine("\n");
+    Console.WriteLine("LastName");
+    client.LastName = Console.ReadLine();
+    Console.WriteLine("\n");
 
-        Console.WriteLine("LastName");
-        client.LastName = Console.ReadLine();
-        Console.WriteLine("\n");
+    Console.WriteLine("CredCard");
+    Console.WriteLine("1 - VISA");
+    Console.WriteLine("2 - MASTERCARD");
+    Console.WriteLine("Or Press Enter");
+    client.CredCard = Console.ReadLine();
 
-        Console.WriteLine("CredCard");
-        Console.WriteLine("1 - VISA");
-        Console.WriteLine("2 - MASTERCARD");
-        Console.WriteLine("Or Press Enter");
-        client.CredCard = Console.ReadLine();
+    reserve.SaveClient(client);
+    allClients++;
 
-        reserve.SaveClient(client);
+    Console.WriteLine("1 - Add a new client");
+    Console.WriteLine("2 - Suite \n");
+    add = int.Parse(Console.ReadLine());
+}
 
-        Console.WriteLine("1 - Add a new client");
-        Console.WriteLine("2 - Suite \n");
-        add = int.Parse(Console.ReadLine());
-    }
+Suite free = new(suiteName: "BE FREE", capacity: 2, dailyValue: 50);
+Suite pro = new(suiteName: "BE PRO", capacity: 4, dailyValue: 100);
+Suite global = new(suiteName: "BE GLOBAL", capacity: 7, dailyValue: 150);
 
-    Suite free = new(suiteName: "BE FREE", capacity: 2, dailyValue: 50);
-    Suite pro = new(suiteName: "BE PRO", capacity: 4, dailyValue: 100);
-    Suite global = new(suiteName: "BE GLOBAL", capacity: 7, dailyValue: 150);
+Console.WriteLine(free.ShowSuite);
+Console.WriteLine(pro.ShowSuite);
+Console.WriteLine(global.ShowSuite + "\n \n");
 
-    Console.WriteLine(free.ShowSuite);
-    Console.WriteLine(pro.ShowSuite);
-    Console.WriteLine(global.ShowSuite + "\n \n");
+Console.WriteLine("Choose a Suite \n");
+Console.WriteLine("1 - BE FREE");
+Console.WriteLine("2 - BE PRO");
+Console.WriteLine("3 - BE HOTEL");
 
-    Console.WriteLine("Choose a Suite \n");
-    Console.WriteLine("1 - BE FREE");
-    Console.WriteLine("2 - BE PRO");
-    Console.WriteLine("3 - BE HOTEL");
+int suite = int.Parse(Console.ReadLine());
 
-    int suite = int.Parse(Console.ReadLine());
+Console.WriteLine("Days \n");
+reserve.ReservedDays = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Days \n");
-    reserve.ReservedDays = int.Parse(Console.ReadLine());
-    var peoples = reserve.AllClient();
 
-    int allClients = 0;
-    for (int i = 0; i < peoples.Count; i++)
-    {
-        allClients++;
-    }
-    if (suite == 1)
-    {
-        reserve.CapacityRoom(free.Capacity, allClients);
-        var total = reserve.TotalDaily(reserve.ReservedDays, free.DailyValue);
-        Console.WriteLine($"People- {allClients} \n" +
-                          $"Suite - {free.SuiteName} \n" +
-                          $"Total - {total}");
 
-    }
-    else if (suite == 2)
-    {
-        reserve.CapacityRoom(pro.Capacity, allClients);
-        var total = reserve.TotalDaily(reserve.ReservedDays, pro.DailyValue);
-        Console.WriteLine($"People- {allClients} \n" +
-                         $"Suite - {pro.SuiteName} \n" +
-                         $"Total - {total}");
-    }
-    else
-    {
-        reserve.CapacityRoom(global.Capacity, allClients);
-        var total = reserve.TotalDaily(reserve.ReservedDays, global.DailyValue);
-        Console.WriteLine($"People- {allClients} \n" +
-                         $"Suite - {global.SuiteName} \n" +
-                         $"Total - {total}");
-    }
+if (suite == 1)
+{
+    reserve.CapacityRoom(free.Capacity, allClients);
+    var total = reserve.TotalDaily(reserve.ReservedDays, free.DailyValue);
+    Console.WriteLine($"People- {allClients} \n" +
+                      $"Suite - {free.SuiteName} \n" +
+                      $"Total - {total}");
 
 }
+else if (suite == 2)
+{
+    reserve.CapacityRoom(pro.Capacity, allClients);
+    var total = reserve.TotalDaily(reserve.ReservedDays, pro.DailyValue);
+    Console.WriteLine($"People- {allClients} \n" +
+                     $"Suite - {pro.SuiteName} \n" +
+                     $"Total - {total}");
+}
+else
+{
+    reserve.CapacityRoom(global.Capacity, allClients);
+    var total = reserve.TotalDaily(reserve.ReservedDays, global.DailyValue);
+    Console.WriteLine($"People- {allClients} \n" +
+                     $"Suite - {global.SuiteName} \n" +
+                     $"Total - {total}");
+}
+
+
